9171a292baeb9dbcdb7fb802ce7c8852babe1f01
Kasaija Kenneth <kasaijak@gmail.com>
2022-11-01 18:40:17 +0300

    Add styles

--- dojo-blog/src/Home.js
+++ dojo-blog/src/Home.js
@@ -1,7 +1,11 @@
 const Home = () => {
+  const Click = () => {
+    console.log('Hi Ken');
+  }
   return (
     <div className="home">
       <h2>Homepage</h2>
+      <button onClick={Click}>button</button>
     </div>
   );
 }
--- dojo-blog/src/Navbar.js
+++ dojo-blog/src/Navbar.js
@@ -1,14 +1,10 @@
 const Navbar = () => {
   return (
     <nav className="navbar">
-      <h1>The Dojo Blog</h1>
+      <h1>The Kenneth Blog</h1>
       <div className="links">
         <a href="/">Home</a>
-        <a href="/create" style={{ 
-          color: 'white', 
-          backgroundColor: '#f1356d',
-          borderRadius: '8px' 
-        }}>New Blog</a>
+        <a href="/create">New Blog</a>
       </div>
     </nav>
   );
