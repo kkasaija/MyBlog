90ca5afcb1a93eb68c2bd229d5b6b850d2adb29d
Kasaija Kenneth <kasaijak@gmail.com>
2022-11-01 17:19:43 +0300

    Modify App.js

--- dojo-blog/src/App.js
+++ dojo-blog/src/App.js
@@ -1,10 +1,18 @@
 import './App.css';
 
 function App() {
+  const title = 'Application Content';
+  const likes = 50;
+  //const person ={name:'Kenneth', age: 50}
+  const arr = [1,2,3,4];
+  const url = 'www.google.com';
   return (
     <div className="App">
       <div className="content">
-
+        <h2>{title}</h2>
+        <p>Liked {likes} times</p>
+        <p>{arr}</p>
+        <a href={url}>Google</a>
       </div>
     </div>
   );
